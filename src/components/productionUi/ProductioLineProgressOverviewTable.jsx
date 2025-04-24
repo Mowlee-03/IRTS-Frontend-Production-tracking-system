9e2837fe77abe945b13b705f16f7db31526dc1ce
Mowlee-03 <mowleemm@gmail.com>
2025-04-24 15:39:15 +0530

    overview resposive alteration

--- src/components/productionUi/ProductioLineProgressOverviewTable.jsx
+++ src/components/productionUi/ProductioLineProgressOverviewTable.jsx
@@ -39,18 +39,17 @@ const ProductionLineProgressOverviewTable = () => {
   const lines = ["Total Orders", "Line 1", "Line 2", "Line 3", "Line 4"]
  
   return (
-    <div className="flex-1  shadow-bg-shadow-1 rounded-xl bg-white">
+    <div className="flex-1 overflow-x-auto shadow-bg-shadow-1 rounded-xl bg-white">
 
       <div className="flex flex-col md:flex-row justify-between items-start md:items-center pl-8 pt-4">
         <div className="flex items-center gap-2 text-gray-700 font-medium">
-          <div className="p-2 xl:p-3  rounded-lg bg-[#F0F3FF] flex items-center justify-center text-[#4F46E5]">
-           <AccessTimeIcon sx={{fontSize:{xs:20 ,md:23,xl:25}}}/>
+          <div className="p-4  rounded-lg bg-[#F0F3FF] flex items-center justify-center text-[#4F46E5]">
+           <AccessTimeIcon/>
           </div>
           <span className="text-lg">In Progress Production Lines</span>
         </div>
       </div>
-
-      <div className="md:pl-14 pr-4 overflow-x-auto">
+      <div className="md:pl-14 pr-4">
       <table className="w-full min-w-[600px] ">
         <thead>
           <tr>
